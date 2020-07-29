d92362a5bc8e74bcc5a61c0048c6bc703c0f3a47
Jokcy <jokcylou@hotmail.com>
2020-07-29 19:40:52 +0800

    jest config outside

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,41 @@
+// module.exports = {
+//   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+// }
+
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    // tell Jest to handle *.vue files
+    'vue',
+    'ts',
+    'tsx',
+  ],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  // support the same @ -> src alias mapping in source code
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom-fifteen',
+  // serializer for snapshots
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
+  // https://github.com/facebook/jest/issues/6766
+  testURL: 'http://localhost/',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
 }
